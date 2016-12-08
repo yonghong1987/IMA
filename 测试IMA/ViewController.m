f6957b17da6859ff7bf7e3d362eebad7a6524069
yonghong1987 <cyh@KavendeMacBook-Pro.local>
2016-12-08 15:56:58 +0800

    txt

--- 测试IMA/ViewController.m
+++ 测试IMA/ViewController.m
@@ -20,7 +20,7 @@
 //    [self.view addSubview:iv];
 //   [iv sd_setImageWithURL:[NSURL URLWithString:@"http://img5.imgtn.bdimg.com/it/u=2253484101,1253799674&fm=21&gp=0.jpg"]];
     
-    NSArray *arr = @[@"2",@"2",@"2",@"2",@"1",@"5",@"==",@"wo是你的",@"jiushinidde"];
+    NSArray *arr = @[@"2",@"2",@"2",@"2",@"1",@"5",@"==",@"wo是你的",@"jiushiniddef"];
     int count = 0;
     for (int i = 0; i < arr.count; i++) {
         if (i == arr.count - 1) {
