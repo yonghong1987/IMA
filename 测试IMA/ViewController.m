fd5fd9dac7e1db3987a084134b9d635c6be9a4fd
yonghong1987 <cyh@KavendeMacBook-Pro.local>
2016-12-08 15:16:01 +0800

    img
    img

--- 测试IMA/ViewController.m
+++ 测试IMA/ViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "ViewController.h"
-
+#import "UIImageView+WebCache.h"
 @interface ViewController ()
 
 @end
@@ -16,6 +16,28 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+//    UIImageView *iv = [[UIImageView alloc]initWithFrame:CGRectMake(100, 100, 100, 100)];
+//    [self.view addSubview:iv];
+//   [iv sd_setImageWithURL:[NSURL URLWithString:@"http://img5.imgtn.bdimg.com/it/u=2253484101,1253799674&fm=21&gp=0.jpg"]];
+    
+    NSArray *arr = @[@"2",@"2",@"2",@"2"];
+    int count = 0;
+    for (int i = 0; i < arr.count; i++) {
+        if (i == arr.count - 1) {
+            if (arr[i] == arr[0]) {
+                count += 1;
+            }
+        }else{
+            if (arr[i] == arr[i + 1]) {
+                count += 1;
+            }
+        }
+    }
+    if (count == arr.count) {
+        NSLog(@"每个值都一样");
+    }else{
+    NSLog(@"每个值都ljdsf");
+    }
     // Do any additional setup after loading the view, typically from a nib.
 }
 
